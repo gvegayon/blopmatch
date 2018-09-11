4fd8e219faf42f4a5bdc46097a767c822d6f44c2
George G. Vega Yon <g.vegayon@gmail.com>
2018-09-11 11:31:19 -0700

    Testing more cases

--- misc/testing-bilevel.R
+++ misc/testing-bilevel.R
@@ -1,7 +1,7 @@
 library(magrittr)
 library(blopmatch)
 
-set.seed(1222)
+set.seed(114444)
 
 k <- 2
 n <- 10
@@ -17,9 +17,14 @@ ans <- blopi_lpsolve(xi, X, D)
 
 ans$lambda %*% X %>% as.matrix() %>% rbind(., xi) %>% dist
 
-plot(X)
-points(xi[1], xi[2], col="red")
-
 # Objective vs matched
-ans$lambda %*% X
+xihat <- ans$lambda %*% X
+
+plot(x=X[,1], y=X[,2],
+     xlim = range(c(X[,1], xi[1])),
+     ylim = range(c(X[,2], xi[2])))
+points(x=xi[1], y=xi[2], col="red")
+points(x=xihat[1], y=xihat[2], col="blue")
+
+xihat
 xi
